24e8da6e2b1e8ac61dfa96cecd279ce37408bc43
Jackson Tian <shyvo1987@gmail.com>
2016-06-07 16:53:52 +0800

    add parameter make for generate cache key

--- index.js
+++ index.js
@@ -11,21 +11,27 @@ var debug = require('debug')('cachex');
  * the get/setex must be an yieldable method
  * @param {String} prefix prefix, used for key
  * @param {String} name method name, used for key
- * @param {Generator|Thunkify} yieldable must be a yieldable object
+ * @param {Generator|Thunkify|Promise} yieldable must be a yieldable object
  * @param {Number} expire the expire time, in seconds
+ * @param {Function} make the function used to generate key
  * @return Generator the new generator, will auto process cache
  */
-module.exports = function (store, prefix, name, yieldable, expire) {
-  return function * () {
+module.exports = function (store, prefix, name, yieldable, expire, make) {
+  return function* () {
     // copy arguments
     var args = new Array(arguments.length);
     for (var i = 0; i < arguments.length; i++) {
       args[i] = arguments[i];
-      if (typeof args[i] === 'object') {
+      if (typeof args[i] === 'object' && typeof make !== 'function') {
         throw new TypeError('use object not fit cache key');
       }
     }
-    var key = prefix + ':' + name + ':' + args.join(':');
+
+    var suffix = typeof make === 'function' ? make.apply(null, args)
+      : args.join(':');
+
+    var key = prefix + ':' + name + ':' + suffix;
+
     var result = yield store.get(key);
     debug('get value for key: %s with cache, value is: %j', key, result);
     if (!result) {
--- test/index.test.js
+++ test/index.test.js
@@ -9,10 +9,10 @@ describe('cachex', function () {
   var inMemory = {};
 
   var store = {
-    get: function * (key) {
+    get: function* (key) {
       return inMemory[key] ? 'sql:from:cache' : '';
     },
-    setex: function * (key, value, expire) {
+    setex: function* (key, value, expire) {
       inMemory[key] = value;
       setTimeout(function () {
         delete inMemory[key];
@@ -24,7 +24,7 @@ describe('cachex', function () {
     setTimeout(callback, s * 1000);
   });
 
-  var query = function * (sql) {
+  var query = function* () {
     return 'sql:from:db';
   };
 
@@ -32,10 +32,34 @@ describe('cachex', function () {
     var queryx = cachex(store, 'test', 'query', query, 1);
     var result = yield queryx('sql');
     expect(result).to.be('sql:from:db');
-    var result = yield queryx('sql');
+    result = yield queryx('sql');
     expect(result).to.be('sql:from:cache');
     yield sleep(1);
-    var result = yield queryx('sql');
+    result = yield queryx('sql');
+    expect(result).to.be('sql:from:db');
+  });
+
+  it('cachex should throw', function* () {
+    var queryx = cachex(store, 'test', 'query', query, 1);
+    try {
+      yield queryx({'sql': 'sql'});
+    } catch (ex) {
+      expect(ex.message).to.be('use object not fit cache key');
+      return;
+    }
+    expect(false).to.be.ok();
+  });
+
+  it('cachex should throw', function* () {
+    var queryx = cachex(store, 'test', 'query', query, 1, function (obj) {
+      return obj.sql;
+    });
+    var result = yield queryx({sql: 'sql'});
+    expect(result).to.be('sql:from:db');
+    result = yield queryx({sql: 'sql'});
+    expect(result).to.be('sql:from:cache');
+    yield sleep(1);
+    result = yield queryx({sql: 'sql'});
     expect(result).to.be('sql:from:db');
   });
 });
